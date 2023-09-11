1bacd4e0a3b931cdc5af36d7e9bab9d6989a3947
tizee <33030965+tizee@users.noreply.github.com>
2023-09-11 19:47:39 +0800

    fix: zeal

--- Formula/zeal.rb
+++ Formula/zeal.rb
@@ -26,14 +26,11 @@ class Zeal < Formula
   end
 
   def install
-    mkdir "build" do
-      cd "build" do
-        system "cmake", "..", *std_cmake_args
-        system "make"
-        prefix.install "bin/Zeal.app"
-        (bin/"zeal").write("#! /bin/sh\n#{prefix}/Zeal.app/Contents/MacOS/Zeal \"$@\"\n")
-      end
-    end
+      system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+      system "cmake", "--build", "build"
+      system "cmake", "--install", "build"
+      prefix.install "build/Zeal.app"
+      (bin/"zeal").write("#! /bin/sh\n#{prefix}/Zeal.app/Contents/MacOS/Zeal \"$@\"\n")
   end
 
   test do
